5f30f3696741756863e2daa12b56bff6f4c0c59c
MrRobz <robinphilipthomas@outlook.in>
2021-05-11 23:38:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fetch = require('node-fetch');
+
+async function main() {
+  try {
+    const tablePromise = await fetch('https://api.airtable.com/v0/appoztB9WNejYQSsR/config-table?maxRecords=1', {
+      method: 'get',
+      headers: { Authorization: `Bearer ${core.getInput('airtable-api')}` }
+    });
+
+    const data = await tablePromise.json();
+
+    const { records } = data;
+
+    const record = records.find(
+      (record) => record.fields.Name === 'canMergeToMain',
+    );
+  
+    const value = record.fields.Value;
+
+    if(!value) {
+      core.setFailed("Merges to main is currently blocked. Pls check in General channel more info!");
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
